e0b4f89e4108ac7be79b8c07312b150c345a80c7
sai <saisankargochhayat@gmail.com>
2020-03-26 10:45:53 -0400

    Changed app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
-import cowsay
+#import cowsay
 import time
+import logging
+import daiquiri
 
 def main():
     """Say hello to Thoth."""
+    daiquiri.setup(level=logging.INFO)
     while True:
-        for character in cowsay.chars:
-            character("Hello, Thoth!")
-            time.sleep(5)
+        logger = daiquiri.getLogger(__name__)
+        logger.info("It works and log to stderr by default with color! Every 10 seconds ;)")
+        time.sleep(10)
+        # for character in cowsay.chars:
+        #     character("Hello, Thoth!")
+        #     time.sleep(5)
 
 
 __name__ == "__main__" and main()
